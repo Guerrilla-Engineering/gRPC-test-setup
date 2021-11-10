19d2ed2ee88ea0a3167f556238c357d6d61c625b
Lalit Tyagi <lalit@guerrillaproduct.group>
2021-11-10 17:18:12 +0530

    grpc server with envoy in docker

---
+++ README.md
@@ -0,0 +1,19 @@
+### Run Locally
+
+RUN
+
+```
+$ docker-compose up -d
+```
+
+```
+localhost:8080
+```
+
+DESCRIPTION
+
+```
+server is running at localhost:9090 in docker container
+
+envoy is runny at localhost:8080 in docker container
+```
---
+++ go/server.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	movie "github.com/sabarivig/grpx/movies"
+
+	"flag"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"net/http"
+)
+
+func unartInceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (resp interface{}, err error) {
+	log.Print("inceptor hit", info.FullMethod)
+	return handler(ctx, req)
+
+}
+
+func cgrpc() {
+	lis, err := net.Listen("tcp", ":9090")
+	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(unartInceptor))
+	if err != nil {
+		log.Fatal("Failed to listen", err)
+	}
+	fmt.Println("starring server  at 9090 ")
+
+	s := movie.Server{}
+
+	movie.RegisterMovieServiceServer(grpcServer, &s)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal("Failed To Serve")
+	}
+
+}
+
+var (
+	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:7899", "gRPC server endpoint")
+)
+
+func rest() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := movie.RegisterMovieServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.ListenAndServe("localhost:8888", mux)
+
+}
+
+func main() {
+	cgrpc()
+	//	go rest()
+}
